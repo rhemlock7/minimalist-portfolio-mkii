cb04f5ac9a745de2d7e0798a1268bbf52482c097
Ryan Hemlock <ryanhemlock@Ashleys-MacBook-Pro.local>
2023-11-19 08:10:21 -0500

    second commit

--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      
+      {/* Test comment */}
       {/* Navigation */}
       <Navigation />
 
