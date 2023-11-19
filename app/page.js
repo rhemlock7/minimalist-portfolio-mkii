80b81ffbfdfe05758e4df5c19823931479013a9f
Ryan Hemlock <ryan@ryanhemlock.com>
2023-11-19 10:37:12 -0500

    created assets folder and added images and logos

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
+import { RyanHeadshot } from '../assets/ryan-photos/ryan-hemlock-headshot.jpg';
 
 export default function Home() {
   return (
     <section>
+      <img src={RyanHeadshot} alt="ryan hemlock headshot" />
       <h1>Software Developer</h1>
     </section>
   )
