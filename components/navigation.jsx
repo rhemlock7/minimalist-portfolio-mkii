61746a75a6bea9b4473aad05bf4550db98dfe1c0
Ryan Hemlock <ryanhemlock@Ashleys-MacBook-Pro.local>
2023-11-19 07:32:08 -0500

    first commit

---
+++ components/navigation.jsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import React from 'react'
+
+function Navigation() {
+  return (
+    <header>
+      <h2>Ryan Hemlock</h2>
+
+      <nav>
+        
+      </nav>
+    </header>
+  )
+}
+
+export default Navigation
