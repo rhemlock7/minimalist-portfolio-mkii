fce53d4adcf52e813a81bf2dd2e254ef767a55fc
Ryan Hemlock <ryanhemlock@Ashleys-MacBook-Pro.local>
2023-11-21 07:25:25 -0500

    Created initial project component

---
+++ components/project.component.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Image from 'next/image';
+import CoolGuyRy from '../public/23-RH-PORTRAIT-EDIT-421 (2).jpg'
+
+const Project = () => {
+  return (
+    <a href=''>
+      <div className='my-8'>
+        <Image src={CoolGuyRy} alt="cool ryan photo" />
+
+        <div className='flex justify-between items-center'>
+          <h5>Project</h5>
+          <p>Project</p>
+        </div>
+      </div>
+    </a>
+  )
+}
+
+export default Project;
