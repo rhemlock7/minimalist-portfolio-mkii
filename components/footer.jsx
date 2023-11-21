e748c4fa3e543cbabf7583b10eb1fc84fd1f01b0
Ryan Hemlock <ryanhemlock@Ashleys-MacBook-Pro.local>
2023-11-21 07:06:36 -0500

    Added footer component

--- components/footer.jsx
+++ components/footer.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer>
-      
+    <footer className='flex flex-col mb-8 pt-10 border-t-2'>
+      <div className='flex flex-col py-4'>
+        <a href='https://github.com/rhemlock7' target='blank' rel='noreferrer'>Github</a>
+        <a href='https://www.linkedin.com/in/ryan-hemlock/' target='blank' rel='noreferrer'>LinkedIn</a>
+      </div>
+
+      <div className='py-4'>
+        <p>Let’s connect: <span>ryan@ryanhemlock.com</span></p>
+      </div>
     </footer>
   )
 }
