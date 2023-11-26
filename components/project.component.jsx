ce8869acb825ac6eceacb681d54bc8e08c042435
Ryan Hemlock <ryanhemlock@Ashleys-MacBook-Pro.local>
2023-11-26 11:04:37 -0500

    Removed arrows from project component

--- components/project.component.jsx
+++ components/project.component.jsx
@@ -1,24 +1,30 @@
+//imports
 import React from 'react'
 import Image from 'next/image';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+
+//images
 import CoolGuyRy from '../public/assets/ryan-photos/23-RH-PORTRAIT-EDIT-421 (2).jpg'
-import Arrow from '../public/assets/icons/arrow-icon.svg';
+//CSS Stylesheet
 import './project.styles.scss';
 
 const Project = () => {
   return (
     <a href=''>
       <div className='project-container'>
+
+        {/* Image */}
         <div className='img-container'>
           <Image src={CoolGuyRy} alt="cool ryan photo" id='project-photo'/>
         </div>
 
+        {/* Text */}
         <div className='flex justify-between items-center mt-1'>
           <h3 className='text-base'>Project</h3>
-          <div>
-            <h3 className='text-gray-500 text-base' id='view'>View</h3>
-            <image src={Arrow} width={100} alt='arrow' id='view-arrow' />
-          </div>
+          <h3 className='text-gray-500 text-base' id='view'>View</h3>          
         </div>
+
       </div>
     </a>
   )
