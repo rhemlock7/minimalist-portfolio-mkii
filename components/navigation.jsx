36d4c38f01bbeed03ee96c2ef12d227a25f304ea
Ryan Hemlock <ryanhemlock@Ashleys-MacBook-Pro.local>
2023-11-26 08:13:26 -0500

    Updated nav and finally ANIMATED it

--- components/navigation.jsx
+++ components/navigation.jsx
@@ -2,7 +2,7 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { useState } from 'react';
-import { useTransition, animated } from 'react-spring';
+import { useSpring, animated } from '@react-spring/web';
 import Hamburger from 'hamburger-react';
 import RyanPhoto from '../public/assets/ryan-photos/ryan-hemlock-headshot.jpg';
 import './navigation.styles.scss';
@@ -10,73 +10,77 @@ import './navigation.styles.scss';
 function Navigation() {
   // Usestate for hamburger menu
   const [open, setOpen] = useState(false);
+
+  // Menu toggle function
   const handleChange = () => {
     setOpen(!open);
+
   }
 
-// Menu component with conditional rendering
-let menu;
-// let menuMask;
-
-if (open) {
-  menu = 
-  <div className='nav-menu-container'>
-
-    {/* About Link */}
-    <ul className='nav-menu'>
-      <li>
-        <a href="#">
-          <div className='li-container'>
-            <h3>About</h3>
-            <Image src={RyanPhoto} width={100} alt='ryan hemlock portrait' className='hidden sm:block rounded-md' />
-          </div>
-        </a>
-      </li>
-
-      {/* Portfolio Link */}
-      <li>
-        <a href="#">
-          <div className='li-container'>
-            <h3>Portfolio</h3>
-            <Image alt='project photo' className='hidden sm:block' />
-          </div>
-        </a>
-      </li>
-
-      {/* Blog Link */}
-      <li>
-        <a href="#">
-          <div className='li-container'>
-            <h3>Blog</h3>
-            <Image alt='blog photo' className='hidden sm:block' />
-          </div>
-        </a>
-      </li>
-    </ul>
-  </div>
-
-  // menuMask = 
-  // <div className='bg-white fixed top-0 h-full w-full -z-10' onClick={() => setOpen(false)}>
+  //Nav-Menu Animation
+  const transition = useSpring({
+    opacity: open ? 1 : 0,
+    transform: open ? 'translateX(0%)' : 'translateX(-100%)'
     
-  // </div>
-}
-
+    // from: { opacity: 0, transform: 'translateX(-100%)' },
+    // enter: { opacity: 1, transform: 'translateX(0%)' },
+    // leave: { opacity: 0, transform: 'translateX(-100%)' },
+});
 
 
   // Navgiation Component
   return (
     <header className=''>
-      <nav className='h-16 flex items-center justify-between lg:pt-6'>
+
+      {/* PRIMARY NAVIGATION */}
+      <nav className='h-16 flex items-center justify-between pt-12'>
         <div className='logo-wrapper'>
           <h2 className='text-lg' id='logo'>Ryan Hemlock</h2>
         </div>
         {/* <p>Dark mode?</p> */}
-        <Hamburger toggled={open} toggle={setOpen} onClick={handleChange} id='hamburger-icon'/>
+        <Hamburger toggled={open} toggle={setOpen} onClick={handleChange} id='hamburger-icon' />
       </nav>
 
-      {/* If "open" is false, open menu */}
-      {/* { menuMask } */}
-      { menu }
+      {/* -------------------------------------- */}
+      {/* MOBILE MENU that displays when toggled */}
+        <animated.div style={transition} className='nav-menu-container'>
+
+          <div className='absolute right-4 md:right-14 top-8'>
+            <Hamburger toggled={open} toggle={setOpen} onClick={handleChange} id='hamburger-icon' />
+          </div>
+
+          {/* About Link */}
+          <ul className='nav-menu'>
+            <li>
+              <a href="#">
+                <div className='li-container'>
+                  <h3>About</h3>
+                  <Image src={RyanPhoto} width={100} alt='ryan hemlock portrait' className='hidden sm:block rounded-md' />
+                </div>
+              </a>
+            </li>
+
+            {/* Portfolio Link */}
+            <li>
+              <a href="#">
+                <div className='li-container'>
+                  <h3>Portfolio</h3>
+                  <Image alt='project photo' className='hidden sm:block' />
+                </div>
+              </a>
+            </li>
+
+            {/* Blog Link */}
+            <li id='final-li-border'>
+              <a href="#">
+                <div className='li-container'>
+                  <h3>Blog</h3>
+                  <Image alt='blog photo' className='hidden sm:block' />
+                </div>
+              </a>
+            </li>
+          </ul>
+      </animated.div>
 
     </header>
   )
